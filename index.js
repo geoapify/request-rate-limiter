1a9e72bf3c9c07036157d508db17213d1cbbcc1b
Oleg Hovsepyan <hovsepyan021@gmail.com>
2024-08-25 20:22:14 +0400

    Adding onBatchCompleteFired() && tests
    *Adding test to make 1000 requests
    *Testing the response of the method
    *Adding tests for onBatchCompleteFired()

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 async function rateLimitedRequests(requests, maxRequests, interval, options) {
+    batchState = {
+        batchItemsToFire: [],
+        batchEndIndex: 0
+    }
     const result = new Array(requests.length);
     const promises = [];
 
@@ -7,15 +11,10 @@ async function rateLimitedRequests(requests, maxRequests, interval, options) {
         const batch = requests.slice(startIndex, endIndex).map((execute, index) =>
             execute().then(res => {
                 result[startIndex + index] = res;
-                if(options && options.batchSize && options.onBatchComplete) {
-                    onBatchCompleteFired(result, startIndex, endIndex, options.batchSize, options.onBatchComplete);
-                }
-                if(options && options.onProgress) {
-                    onProgressFired(result, startIndex, endIndex, options.onProgress);
-                }
+                return res;
             })
         );
-
+        onBatchFinish(batchState, batch, options, startIndex, endIndex);
         promises.push(...batch);
 
         if (endIndex < requests.length) {
@@ -27,12 +26,34 @@ async function rateLimitedRequests(requests, maxRequests, interval, options) {
     return result;
 }
 
-function onBatchCompleteFired(result, startIndex, endIndex, batchSize, onBatchComplete) {
-    //     TODO: implement this part
+function onBatchCompleteFired(batchState, result, startIndex, endIndex, batchSize, onBatchComplete) {
+    batchState.batchItemsToFire.push(...result);
+
+    while (batchState.batchItemsToFire.length >= batchSize) {
+        const batch = batchState.batchItemsToFire.splice(0, batchSize);
+        const result = {
+            startIndex: batchState.batchEndIndex,
+            stopIndex: batchState.batchEndIndex + batchSize - 1,
+            results: batch
+        }
+        batchState.batchEndIndex += batchSize;
+        onBatchComplete(result);
+    }
 }
 
 function onProgressFired(result, startIndex, endIndex, onProgress) {
     //     TODO: implement this part
+}
+
+function onBatchFinish(batchState, batch, options, startIndex, endIndex) {
+    Promise.all(batch).then(result => {
+        if (options && options.batchSize && options.onBatchComplete) {
+            onBatchCompleteFired(batchState, result, startIndex, endIndex, options.batchSize, options.onBatchComplete);
+        }
+        if (options && options.onProgress) {
+            onProgressFired(result, startIndex, endIndex, options.onProgress);
+        }
+    })
 }
 
 module.exports = rateLimitedRequests
--- test/rateLimitedRequests.test.js
+++ test/rateLimitedRequests.test.js
@@ -1,62 +1,122 @@
 const rateLimitedRequests = require('../index');
 
-jest.setTimeout(30000);
+jest.setTimeout(60000);
 
 const maxRetries = 2; // number of retries, if it is 2, then in total we will see 3 requests (1 initial + 2 retries)
 const retryDelayInMillis = 500;
-const requestURL = `https://www.google.com/`;
+const requestURL = `https://httpbin.org/get`;
 
-test('execute 100 requests', async () => {
-    const requests = generateRequests(100);
+test('execute 50 requests', async () => {
+    const requests = generateRequests(50);
 
     const options = {
         batchSize: null,
-        onProgress: (progress) => {
-            console.log(progress);
-        },
-        onBatchComplete: (batch) => {
-            console.log(batch);
-        }
+        onProgress: null,
+        onBatchComplete: null
     }
     let result = await rateLimitedRequests(requests, 25, 1000, options);
-    expect(result.length).toBe(100);
+    expect(result.length).toBe(50);
+    expectResponseToContainData(result, 0, 49);
 });
 
-test('execute 100 requests with batch', async () => {
-    const requests = generateRequests(100);
+test('execute 1000 requests', async () => {
+    const requests = generateRequests(1000);
 
+    const options = {
+        batchSize: null,
+        onProgress: null,
+        onBatchComplete: null
+    }
+    let result = await rateLimitedRequests(requests, 100, 1000, options);
+    expect(result.length).toBe(1000);
+    expectResponseToContainData(result, 0, 999);
+});
+
+test('execute 50 requests with batch', async () => {
+    const requests = generateRequests(50);
+    const batchItems = [];
     const options = {
         batchSize: 10,
         onProgress: () => {
         },
-        onBatchComplete: () => {
+        onBatchComplete: (batch) => {
+            batchItems.push(batch)
         }
     }
     let result = await rateLimitedRequests(requests, 25, 1000, options);
-    expect(result.length).toBe(100);
-//     TODO: finalize test
+    expect(result.length).toBe(50);
+    expect(batchItems.length).toBe(5);
+    expectBatchResponseToContainData(batchItems, 0,0,9);
+    expectBatchResponseToContainData(batchItems, 1,10,19);
+    expectBatchResponseToContainData(batchItems, 2,20,29);
+    expectBatchResponseToContainData(batchItems, 3,30,39);
+    expectBatchResponseToContainData(batchItems, 4,40,49);
 });
+
+test('execute 50 requests with batch (batchSize bigger than total requests)', async () => {
+    const requests = generateRequests(50);
+    const batchItems = [];
+    const options = {
+        batchSize: 60,
+        onProgress: () => {
+        },
+        onBatchComplete: (batch) => {
+            batchItems.push(batch)
+        }
+    }
+    let result = await rateLimitedRequests(requests, 25, 1000, options);
+    expect(result.length).toBe(50);
+    expect(batchItems.length).toBe(0);
+});
+
+test('execute 50 requests with batch (batchSize equals to total requests)', async () => {
+    const requests = generateRequests(50);
+    const batchItems = [];
+    const options = {
+        batchSize: 50,
+        onProgress: () => {
+        },
+        onBatchComplete: (batch) => {
+            batchItems.push(batch)
+        }
+    }
+    let result = await rateLimitedRequests(requests, 25, 1000, options);
+    expect(result.length).toBe(50);
+    expect(batchItems.length).toBe(1);
+    expectBatchResponseToContainData(batchItems, 0,0,49);
+});
+
+function expectResponseToContainData(result, startIndex, endIndex) {
+    for(let i = startIndex; i <= endIndex; i++){
+        expect(JSON.stringify(result[i - startIndex])).toContain(`{"args":{"id":"${i}"}`);
+    }
+}
 
 function generateRequests(numberOfRequests) {
     let result = [];
     for(let i = 0; i < numberOfRequests; i++) {
-        result.push( () => makeRequest());
+        result.push( () => makeRequest(i));
     }
     return result;
 }
 
-async function makeRequest(attempt = 1) {
+function expectBatchResponseToContainData(batchItems, index, startIndex, endIndex) {
+    expectResponseToContainData(batchItems[index].results, startIndex, endIndex);
+    expect(batchItems[index].startIndex).toBe(startIndex);
+    expect(batchItems[index].stopIndex).toBe(endIndex);
+}
+
+async function makeRequest(requestId, attempt = 1) {
     try {
-        const response = await fetch(requestURL);
+        const response = await fetch(requestURL + '?id=' + requestId);
         if (!response.ok) {
             throw new Error(`HTTP error occured Status: ${response.status}`);
         }
-        const data = await response.json();
-        return data.features.length ? data.features[0] : {error: "Not found"};
+        return await response.json();
     } catch (error) {
         if (attempt <= maxRetries) {
             await new Promise(resolve => setTimeout(resolve, retryDelayInMillis));
-            return makeRequest(attempt + 1);
+            return makeRequest(requestId, attempt + 1);
         } else {
             return {error: error.toString()};
         }
